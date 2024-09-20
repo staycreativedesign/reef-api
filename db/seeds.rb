6a3be0b7f45d35e91d8a80ed55e0fe0368303780
Gus P <gustavoanalytics@gmail.com>
2024-09-20 12:18:30 -0400

    setup: Setup seed data from stores, items, ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+7.times do
+  Store.create(name: Faker::Restaurant.name, description: Faker::Restaurant.description, address: Faker::Address.full_address)
+end
+
+32.times do
+  Item.create(store_id: rand(1..Store.count), name: Faker::Food.dish, description: Faker::Food.description, price_cents: 613)
+end
+
+50.times do
+  Ingredient.create(item_id: rand(1..Item.count), name: Faker::Food.ingredient, quantity: rand(1..8))
+end
